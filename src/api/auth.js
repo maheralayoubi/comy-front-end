753433a86d13b3ac589f67224ac6bfeed927244b
Alzin Mohamad Ghaith <35889862+alzin@users.noreply.github.com>
2024-08-22 03:57:25 +0000

    refactor auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,38 +2,25 @@ import axios from 'axios';
 
 const API_URL = 'https://comy-api.vercel.app/auth';
 
-export const registerUser = async (userData) => {
-    try {
-        const response = await axios.post(`${API_URL}/register`, userData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json',
-            },
-        });
-        return { data: response.data, status: response.status };
-    } catch (error) {
-        if (error.response) {
-            return { data: error.response.data, status: error.response.status };
-        } else {
-            throw error;
-        }
-    }
+const headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
 };
 
-export const loginUser = async (userData) => {
-    try {
-        const response = await axios.post(`${API_URL}/login`, userData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json',
-            },
-        });
-        return { data: response.data, status: response.status };
-    } catch (error) {
-        if (error.response) {
-            return { data: error.response.data, status: error.response.status };
-        } else {
-            throw error;
-        }
+const handleApiResponse = async (apiCall) => {
+  try {
+    const response = await apiCall();
+    return { data: response.data, status: response.status };
+  } catch (error) {
+    if (error.response) {
+      return { data: error.response.data, status: error.response.status };
     }
+    throw error;
+  }
 };
+
+export const registerUser = (userData) =>
+  handleApiResponse(() => axios.post(`${API_URL}/register`, userData, { headers }));
+
+export const loginUser = (userData) =>
+  handleApiResponse(() => axios.post(`${API_URL}/login`, userData, { headers }));
