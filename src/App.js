aaa56716b876c5904a81ab567c4e5ee1dca9fdd1
Maher <maher91syr@gmail.com>
2024-09-04 00:01:53 +0900

    adding terms of use component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import MailConfirmation from './pages/MailConfirmation';
+import TermsOfUse from './pages/TermsOfUse';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/mail-confirmation" element={<MailConfirmation />} />
+        <Route path="/terms-of-use" element={<TermsOfUse />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/TermsOfUse.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TermsOfUse from '../components/TermsOfUse';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Register = () => {
+    return (
+        <div>
+            <Header />
+            <TermsOfUse />
+            <Footer />
+        </div>
+    );
+};
+
+export default Register;
