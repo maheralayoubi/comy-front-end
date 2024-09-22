207b7bdb731f24362f58835469b101274d44de93
rezkaudi <audirezk@gmail.com>
2024-09-22 22:24:52 +0300

    fix merge issue

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,58 +1,73 @@
-import "./styles/Cards.scss";
-import { useEffect, useState } from "react";
+import "./styles/Cards.scss"
+import { useState } from "react"
 
-export const CardTitle = ({ title, theme }) => {
-  return (
-    <div
-      className="cardTitle"
-      style={{ backgroundColor: theme ? theme : "#aaa" }}
-    >
-      {title}
-    </div>
-  );
-};
+export const SectionTitle = ({ title, theme }) => {
+    return (
+        <div className="sectionTitle" style={{ backgroundColor: theme ? theme : "#aaa" }}>
+            {title}
+        </div>
+    )
+}
+
+export const CardTitle = ({ title, theme, name }) => {
+    return (
+        <div className="cardTitle" style={{ backgroundColor: theme ? theme : "#aaa" }}>
+            <img src={`/images/${name}.png`} alt={name} />
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export const CardData = ({ rank, data }) => {
+    return (
+        <div className="valueContainer">
+            {rank && <img src={`/images/rank${rank}.png`} alt={`rank${rank}`} />}
+            <p>{data ? data : "データがありません"}</p>
+        </div>
+    )
+}
 
 export const Card = ({ title, data, theme }) => {
-  return (
-    <div className="card">
-      <CardTitle title={title} theme={theme} />
-      <pre className="cardContent">
-        {data || <span className="no-content">データがありません</span>}
-      </pre>
-    </div>
-  );
-};
+    const lines = data?.split('\n');
+
+    return (
+        <div className="card">
+            <SectionTitle title={title} theme={theme} />
+            <div className="cardContent">
+                {data ?
+                    lines.map((item, index) =>
+                        <p key={index}>{item}</p>
+                    )
+                    : <span className="no-content">データがありません</span>}
+            </div>
+        </div >
+    )
+}
+
 
 export const ChildCard = ({ title, data }) => {
-  const [showMore, setShowMore] = useState(0);
-  const [lines, setlLines] = useState(0);
+    const [showMore, setShowMore] = useState(0);
+    const lines = data?.split('\n');
 
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
+    const toggleShowMore = () => {
+        setShowMore(!showMore);
+    };
 
-  useEffect(() => {
-    if (showMore) setlLines(data.split("\n").length);
-    else setlLines(4);
-  }, [showMore, data]);
 
-  return (
-    <div className="ChildCard">
-      <span className="title">{title}</span>
-      <pre
-        className="cardContent"
-        style={{ WebkitLineClamp: lines, lineClamp: lines }}
-      >
-        {data || "データがありません"}
-      </pre>
-      <div className="showMore" onClick={toggleShowMore}>
-        <img
-          style={{ rotate: `${180 * !showMore}deg` }}
-          src="/images/arrow.png"
-          alt="arrow"
-        />
-        <span>一部を表示</span>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="ChildCard">
+            <span className="title">{title}</span>
+            <div className="cardContent" style={showMore ? {} : { "WebkitLineClamp": 4, "lineClamp": 4 }}>
+                {data ?
+                    lines.map((item, index) =>
+                        <p key={index}>{item}</p>
+                    )
+                    : <span className="no-content">データがありません</span>}
+            </div>
+            {data && <div className="showMore" onClick={toggleShowMore}>
+                <img style={{ rotate: `${180 * !showMore}deg` }} src="/images/arrow.png" alt="arrow" />
+                <span>一部を表示</span>
+            </div>}
+        </div >
+    )
+}
