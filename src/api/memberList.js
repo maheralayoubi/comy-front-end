0311a524544697fe2cc2cf9219615e208efae9ff
Maher <maher91syr@gmail.com>
2024-09-29 23:40:49 +0900

    Define the getSearchResults function

--- src/api/memberList.js
+++ src/api/memberList.js
@@ -5,3 +5,8 @@ export const getMemberList = () =>
     handleApiResponse(() =>
         secureApi.get(`${API_URL}/user/all`)
     );
+
+export const getSearchResults = (query) =>
+    handleApiResponse(() =>
+        secureApi.get(`${API_URL}/user/search?q=${encodeURIComponent(query)}`)
+    );    
