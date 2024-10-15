f290d258ffcf5f40110f7025617518ee7b3aaf97
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-15 15:12:57 +0300

    Test: Leave the “名前” field empty, fill in other fields, and verify that the register button stays disabled.

--- cypress/e2e/Signup page.cy.js
+++ cypress/e2e/Signup page.cy.js
@@ -8,39 +8,53 @@ const typeInInput = (id,value) => {
   return cy.get(id).should('exist').type(value)
 }
 
-describe('Check Page Elements:' , () => {
+// describe('Check Page Elements:' , () => {
+//   beforeEach(() => {
+//     cy.visit('/register')
+//   })
+
+//   it('Verify that the “新規アカウント登録” (New Account Registration) heading is visible.' , () => {
+//     cy.contains("新規アカウント登録");
+//     cy.get('h2').should('be.visible');
+//   })
+
+//   it('Ensure the “名前” (Name) input field is present and accepts input.' , () => {
+//     typeInInput('#name',name).should('have.value', name)
+//   })
+
+//   it('Ensure the “カテゴリー” (Category) input field is present and accepts input.' , () => {
+//     typeInInput('#category',category).should('have.value', category)
+//   })
+
+//   it('Ensure the “メールアドレス” (Email Address) input field is present and accepts input.' , () => {
+//     typeInInput('#email',email).should('have.value', email);
+//   })
+
+//   it('Verify that the two password fields (for password and re-enter password) are present and accept input.' , () => {
+//     typeInInput(`#password`, password).should('have.value', password)
+//     typeInInput('#confirmPassword', password).should('have.value', password)
+//   })
+
+//   it('Verify the “新規アカウント登録” (Register) button is present and disabled initially.' , () => {
+//     cy.get('button[type="submit"]').should('be.visible').should('be.disabled')
+//   })
+
+//   it('Verify the “ログインはこちら” (Go to Login) link is present and clickable.' , () => {
+//     cy.get('a[href="/login"]').should('be.visible').click();
+//     cy.url().should("include", '/login')
+//   })
+// })
+
+describe('Input Field Validation:' , () => {
   beforeEach(() => {
     cy.visit('/register')
   })
 
-  it('Verify that the “新規アカウント登録” (New Account Registration) heading is visible.' , () => {
-    cy.contains("新規アカウント登録");
-    cy.get('h2').should('be.visible');
-  })
-
-  it('Ensure the “名前” (Name) input field is present and accepts input.' , () => {
-    typeInInput('#name',name).should('have.value', name)
-  })
-
-  it('Ensure the “カテゴリー” (Category) input field is present and accepts input.' , () => {
-    typeInInput('#category',category).should('have.value', category)
-  })
-
-  it('Ensure the “メールアドレス” (Email Address) input field is present and accepts input.' , () => {
-    typeInInput('#email',email).should('have.value', email);
-  })
-
-  it('Verify that the two password fields (for password and re-enter password) are present and accept input.' , () => {
-    typeInInput(`#password`, password).should('have.value', password)
-    typeInInput('#confirmPassword', password).should('have.value', password)
-  })
-
-  it('Verify the “新規アカウント登録” (Register) button is present and disabled initially.' , () => {
-    cy.get('button[type="submit"]').should('be.visible').should('be.disabled')
-  })
-
-  it('Verify the “ログインはこちら” (Go to Login) link is present and clickable.' , () => {
-    cy.get('a[href="/login"]').should('be.visible').click();
-    cy.url().should("include", '/login')
+  it('Leave the “名前” field empty, fill in other fields, and verify that the register button stays disabled.' , () => {
+    typeInInput('#category' , category);
+    typeInInput('#email' , email);
+    typeInInput('#password' , password);
+    typeInInput('#confirmPassword' , password)
+    cy.get('button[type="submit"]').should('be.disabled')
   })
 })
