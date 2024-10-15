69046ceebf38783ad7251b39411578e264ee8921
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-15 15:59:31 +0300

    Test: Leave the “カテゴリー” field empty, fill in other fields, and check if the register button stays disabled.

--- cypress/e2e/Signup page.cy.js
+++ cypress/e2e/Signup page.cy.js
@@ -61,4 +61,12 @@ describe('Input Field Validation:' , () => {
   it('Try entering more than 30 characters into the “名前” field and ensure the form either prevents it.', () => {
     typeInInput('#name', "A".repeat(40)).invoke('val').should('have.length.at.most', 30)
   })
+
+  it('Leave the “カテゴリー” field empty, fill in other fields, and check if the register button stays disabled.', () => {
+    typeInInput('#name' , name);
+    typeInInput('#category' , category);
+    typeInInput('#password' , password);
+    typeInInput('#confirmPassword' , password)
+    cy.get('button[type="submit"]').should('be.disabled')
+  })
 })
