c6613b473b261f7cf1c8afe9e6a7c89d70c90694
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-15 14:49:52 +0300

    Improve Code

--- cypress/e2e/Signup page.cy.js
+++ cypress/e2e/Signup page.cy.js
@@ -4,6 +4,10 @@ const category = "Frontend Developer"
 const email = "hakamha8@gmail.com";
 const password = 'Hakamaldeen17'
 
+const typeInInput = (id,value) => {
+  return cy.get(id).should('exist').type(value)
+}
+
 describe('Check Page Elements:' , () => {
   beforeEach(() => {
     cy.visit('/register')
@@ -15,20 +19,20 @@ describe('Check Page Elements:' , () => {
   })
 
   it('Ensure the “名前” (Name) input field is present and accepts input.' , () => {
-    cy.get('#name').should('exist').type(name).should('have.value', name)
+    typeInInput('#name',name).should('have.value', name)
   })
 
   it('Ensure the “カテゴリー” (Category) input field is present and accepts input.' , () => {
-    cy.get('#category').should('exist').type(category).should('have.value', category)
+    typeInInput('#category',category).should('have.value', category)
   })
 
   it('Ensure the “メールアドレス” (Email Address) input field is present and accepts input.' , () => {
-    cy.get('#email').should('exist').type(email).should('have.value', email);
+    typeInInput('#email',email).should('have.value', email);
   })
 
   it('Verify that the two password fields (for password and re-enter password) are present and accept input.' , () => {
-    cy.get("#password").should('exist').type(password).should('have.value', password)
-    cy.get('#confirmPassword').should('exist').type(password).should('have.value', password)
+    typeInInput(`#password`, password).should('have.value', password)
+    typeInInput('#confirmPassword', password).should('have.value', password)
   })
 
   it('Verify the “新規アカウント登録” (Register) button is present and disabled initially.' , () => {
