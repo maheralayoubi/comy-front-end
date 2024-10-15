182194a22af7133f5b7d62a206d96f6077949168
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-15 11:16:27 +0300

    Test: Ensure the “名前” (Name) input field is present and accepts input

--- cypress/e2e/Signup page.cy.js
+++ cypress/e2e/Signup page.cy.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-undef */
+const email = "hakamha8@gmail.com";
+const password = 'Hakamaldeen17'
 
 describe('Check Page Elements:' , () => {
   beforeEach(() => {
     cy.visit('/register')
   })
+
   it('Verify that the “新規アカウント登録” (New Account Registration) heading is visible.' , () => {
     cy.contains("新規アカウント登録");
     cy.get('h2').should('be.visible');
   })
+
+  it('Ensure the “名前” (Name) input field is present and accepts input.' , () => {
+    cy.get('#name').should('exist').type('Hakam aldeen').should('have.value', 'Hakam aldeen')
+  })
+  
 })
