9ced2aab1d7349e751f7bcd4131446d161e4dadd
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-17 10:21:08 +0300

    Test: Leave the email field empty and verify that the “送信” button remains disabled.

--- cypress/e2e/Password Reset Email.cy.js
+++ cypress/e2e/Password Reset Email.cy.js
@@ -1,21 +1,32 @@
 /* eslint-disable no-undef */
 const email = 'hakamha8@gmail.com';
 
-describe('Check Page Elements:' , () => {
+// describe('Check Page Elements:' , () => {
+//   beforeEach(() => {
+//     cy.visit('/forgot-password')
+//   })
+
+//   it('Verify that the “新しいパスワード” heading is visible.' , () => {
+//     cy.contains("新しいパスワード");
+//     cy.get("h2").should("be.visible");
+//   })
+
+//   it('Ensure the “メールアドレス” input field is present and accepts input.' , () => {
+//     cy.get('#email').should('exist').type(email).should('have.value', email)
+//   })
+
+//   it('Verify the “送信” button is visible and disabled initially.' , () => {
+//     cy.get('button[type="submit"]').should('exist').should("be.disabled")
+//   })
+// })
+
+describe('Email Input Validation:' , () => {
   beforeEach(() => {
     cy.visit('/forgot-password')
   })
 
-  it('Verify that the “新しいパスワード” heading is visible.' , () => {
-    cy.contains("新しいパスワード");
-    cy.get("h2").should("be.visible");
-  })
-
-  it('Ensure the “メールアドレス” input field is present and accepts input.' , () => {
-    cy.get('#email').should('exist').type(email).should('have.value', email)
-  })
-
-  it('Verify the “送信” button is visible and disabled initially.' , () => {
-    cy.get('button[type="submit"]').should('exist').should("be.disabled")
+  it('Leave the email field empty and verify that the “送信” button remains disabled.' , () => {
+    cy.get('#email').clear()
+    cy.get('button[type="submit"]').should('be.disabled')
   })
 })
