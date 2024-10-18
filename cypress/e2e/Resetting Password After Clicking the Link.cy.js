4582608ae8d35084fd3a42ff7bd126edc9f60a1c
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-18 11:44:16 +0300

    Test: Leave both password fields empty and check that the “パスワードを更新” button is disabled.

--- cypress/e2e/Resetting Password After Clicking the Link.cy.js
+++ cypress/e2e/Resetting Password After Clicking the Link.cy.js
@@ -4,26 +4,38 @@ const typeInInput = (id, value) => {
   return cy.get(id).should('exist').type(value);
 }
 
-describe('Check Page Elements:' , () => {
+// describe('Check Page Elements:' , () => {
+//   beforeEach(() => {
+//     cy.visit('/reset-password')
+//   })
+
+//   it('Verify that the “パスワードの再設定” (Password Reset) heading is visible.' , () => {
+//     cy.contains('パスワードの再設定')
+//     cy.get('h2').should('be.visible')
+//   })
+
+//   it('Ensure that both “新しいパスワード” (New Password) and “新しいパスワードを再入力” (Re-enter New Password) input fields are present and accept input.' , () => {
+//     typeInInput('#newPassword' , newPassword).should('have.value', newPassword)
+//     typeInInput("#confirmNewPassword" , newPassword).should('have.value', newPassword)
+//   })
+
+//   it('Verify that both password fields have the visibility toggle icon to show/hide the password.', () => {
+//     cy.get('.password-toggle').should("be.visible").should('exist')
+//   })
+
+//   it('Verify that the “パスワードを更新” (Update Password) button is visible and disabled initially.' , () => {
+//     cy.get('button[type="submit"]').should("be.visible").should('exist').should('be.disabled')
+//   })
+// })
+
+describe('Password Field Validation:' ,() =>{
   beforeEach(() => {
     cy.visit('/reset-password')
   })
 
-  it('Verify that the “パスワードの再設定” (Password Reset) heading is visible.' , () => {
-    cy.contains('パスワードの再設定')
-    cy.get('h2').should('be.visible')
-  })
-
-  it('Ensure that both “新しいパスワード” (New Password) and “新しいパスワードを再入力” (Re-enter New Password) input fields are present and accept input.' , () => {
-    typeInInput('#newPassword' , newPassword).should('have.value', newPassword)
-    typeInInput("#confirmNewPassword" , newPassword).should('have.value', newPassword)
-  })
-
-  it('Verify that both password fields have the visibility toggle icon to show/hide the password.', () => {
-    cy.get('.password-toggle').should("be.visible").should('exist')
-  })
-
-  it('Verify that the “パスワードを更新” (Update Password) button is visible and disabled initially.' , () => {
-    cy.get('button[type="submit"]').should("be.visible").should('exist').should('be.disabled')
+  it('Leave both password fields empty and check that the “パスワードを更新” button is disabled.' , () => {
+    cy.get('#newPassword').clear()
+    cy.get('#confirmNewPassword').clear()
+    cy.get('button[type="submit"]').should('be.disabled')
   })
 })
