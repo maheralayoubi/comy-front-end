feb9063bf0f199d8c02bd1141b16640688e574f9
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-18 11:31:40 +0300

    Test: Ensure that both “新しいパスワード” (New Password) and “新しいパスワードを再入力” (Re-enter New Password) input fields are present and accept input.

--- cypress/e2e/Resetting Password After Clicking the Link.cy.js
+++ cypress/e2e/Resetting Password After Clicking the Link.cy.js
@@ -1,8 +1,21 @@
 /* eslint-disable no-undef */
+const newPassword = 'TestPassword123'
+const typeInInput = (id, value) => {
+  return cy.get(id).should('exist').type(value);
+}
 
 describe('Check Page Elements:' , () => {
+  beforeEach(() => {
+    cy.visit('/reset-password')
+  })
+
   it('Verify that the “パスワードの再設定” (Password Reset) heading is visible.' , () => {
     cy.contains('パスワードの再設定')
     cy.get('h2').should('be.visible')
+  })
+
+  it('Ensure that both “新しいパスワード” (New Password) and “新しいパスワードを再入力” (Re-enter New Password) input fields are present and accept input.' , () => {
+    typeInInput('#newPassword' , newPassword).should('have.value', newPassword)
+    typeInInput("#confirmNewPassword" , newPassword).should('have.value', newPassword)
   })
 })
