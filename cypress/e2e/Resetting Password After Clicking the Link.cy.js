94bababa73c8fb5a7420f3fef3dd4ed22bc43aa5
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-19 10:50:00 +0300

    Test: Password Visibility Toggle

--- cypress/e2e/Resetting Password After Clicking the Link.cy.js
+++ cypress/e2e/Resetting Password After Clicking the Link.cy.js
@@ -28,28 +28,50 @@ const typeInInput = (id, value) => {
 //   })
 // })
 
-describe('Password Field Validation:' ,() =>{
+// describe('Password Field Validation:' ,() =>{
+//   beforeEach(() => {
+//     cy.visit('/reset-password')
+//   })
+
+//   it('Leave both password fields empty and check that the “パスワードを更新” button is disabled.' , () => {
+//     cy.get('#newPassword').clear()
+//     cy.get('#confirmNewPassword').clear()
+//     cy.get('button[type="submit"]').should('be.disabled')
+//   })
+
+//   it('Enter a password shorter than the minimum required length (e.g., less than 8 characters) and verify that an error message is shown.' , () => {
+//     typeInInput('#newPassword', 'fdfd')
+//     typeInInput('#confirmNewPassword', 'fdfd')
+//     cy.get('button[type="submit"]').click()
+//     cy.contains('パスワードは8文字以上で、数字と大文字を含む必要があります。')
+//   })
+
+//   it('Enter different values in the “新しいパスワード” and “新しいパスワードを再入力” fields and verify that an error message is displayed.' , () => {
+//     typeInInput('#newPassword', newPassword)
+//     typeInInput('#confirmNewPassword', "password123")
+//     cy.get('button[type="submit"]').click()
+//     cy.contains("パスワードが一致しません。")
+// })
+// })
+
+describe('Password Visibility Toggle:' , () => {
   beforeEach(() => {
     cy.visit('/reset-password')
   })
 
-  it('Leave both password fields empty and check that the “パスワードを更新” button is disabled.' , () => {
-    cy.get('#newPassword').clear()
-    cy.get('#confirmNewPassword').clear()
-    cy.get('button[type="submit"]').should('be.disabled')
+  it('Verify that both password fields are masked by default.' , () => {
+    typeInInput('#newPassword', newPassword).should("have.attr", "type", "password");
+    typeInInput('#confirmNewPassword', newPassword).should("have.attr", "type", "password");
   })
 
-  it('Enter a password shorter than the minimum required length (e.g., less than 8 characters) and verify that an error message is shown.' , () => {
-    typeInInput('#newPassword', 'fdfd')
-    typeInInput('#confirmNewPassword', 'fdfd')
-    cy.get('button[type="submit"]').click()
-    cy.contains('パスワードは8文字以上で、数字と大文字を含む必要があります。')
-  })
-
-  it('Enter different values in the “新しいパスワード” and “新しいパスワードを再入力” fields and verify that an error message is displayed.' , () => {
-    typeInInput('#newPassword', newPassword)
-    typeInInput('#confirmNewPassword', "password123")
-    cy.get('button[type="submit"]').click()
-    cy.contains("パスワードが一致しません。")
-})
+  it("Click the visibility toggle icon and ensure that the password becomes visible (not masked) & Click the icon again to ensure the password is masked again.", () => {
+    typeInInput('#newPassword', newPassword).should("have.attr", "type", "password");
+    typeInInput('#confirmNewPassword', newPassword).should("have.attr", "type", "password");
+    cy.get(".password-toggle").first().click();
+    cy.get("#newPassword").should("have.attr", "type", "text");
+    cy.get("#confirmNewPassword").should("have.attr", "type", "text");
+    cy.get(".password-toggle").first().click();
+    cy.get("#newPassword").should("have.attr", "type", "password");
+    cy.get("#confirmNewPassword").should("have.attr", "type", "password");
+  });
 })
