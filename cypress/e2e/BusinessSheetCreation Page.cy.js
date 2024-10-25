ca6df5b8a458ea7681d2c1966b59bc1246c6c52f
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-10-25 11:53:45 +0300

    Test: Verify that each step renders the appropriate input elements (TextArea, Input, UploadImage, Fonts, Themes).

--- cypress/e2e/BusinessSheetCreation Page.cy.js
+++ cypress/e2e/BusinessSheetCreation Page.cy.js
@@ -36,7 +36,137 @@ const typeInInput = (id, value) => {
   return cy.get(id).type(value);
 };
 
-describe('Initial Load:', () => {
+const clickAndCheckTextarea = () => {
+  cy.get('textarea').should('exist');
+  cy.get('.btn.dark').click();
+};
+
+const checkInputs = (ids) => {
+  ids.forEach(id => {
+    cy.get(`#${id}`).should('exist');
+  });
+};
+
+
+// describe('Initial Load:', () => {
+//   beforeEach(() => {
+//     cy.visit('/login');
+//     typeInInput('#email', email);
+//     typeInInput('#password', password);
+//     cy.get('button[type=submit]').click();
+//     cy.wait(10000).visit('business-sheet-creation');
+//   });
+
+//   it('Verify that the BusinessSheetCreation page loads without any errors.', () => {
+//     cy.url().should('include', 'business-sheet-creation');
+//   });
+
+//   it('Verify that the form data is initialized correctly from local storage (or with default values if not present).', () => {
+//     const formData = {
+//       shortBiography,
+//       businessDescription,
+//       personalInformation,
+//       goals,
+//       accomplishments,
+//       interests,
+//       networks,
+//       skills,
+//       goldenEggs: {
+//         goldenEgg0,
+//         goldenEgg1,
+//         goldenEgg2
+//       },
+//       goldenGooses: {
+//         goldenGoose0,
+//         goldenGoose1,
+//         goldenGoose2
+//       },
+//       goldenFarmers: {
+//         goldenFarmer0,
+//         goldenFarmer1,
+//         goldenFarmer2
+//       },
+//       companyStrengths,
+//       powerWords: {
+//         powerWords0,
+//         powerWords1,
+//         powerWords2,
+//         powerWords3,
+//         powerWords4,
+//         powerWords5
+//       },
+//       itemsProducts: {
+//         itemsProducts0,
+//         itemsProducts1,
+//         itemsProducts2
+//       }
+//     };
+//     localStorage.setItem('businessFormData', JSON.stringify(formData));
+//     cy.reload();
+
+//     const storedValue = JSON.parse(localStorage.getItem('businessFormData'));
+//     cy.log(storedValue);
+    
+//     // Fill the form for the main text fields
+//     ['shortBiography', 'businessDescription', 'personalInformation', 'goals', 'accomplishments', 'interests', 'networks', 'skills'].forEach(key => {
+//       typeInInput(`textarea[name=${key}]`, storedValue[key]);
+//       cy.get(`textarea[name=${key}]`).should('have.value', storedValue[key]);
+//       cy.get('.btn.dark').click();
+//     });
+    
+//     // Fill the form for goldenEggs
+//     Object.keys(storedValue.goldenEggs).forEach(key => {
+//       typeInInput(`#${key}`, storedValue.goldenEggs[key]);
+//       cy.get(`#${key}`).should('have.value', storedValue.goldenEggs[key]);
+//     });
+//     cy.get('.btn.dark').click();
+    
+//     // Fill the form for goldenGooses
+//     Object.keys(storedValue.goldenGooses).forEach(key => {
+//       typeInInput(`#${key}`, storedValue.goldenGooses[key]);
+//       cy.get(`#${key}`).should('have.value', storedValue.goldenGooses[key]);
+//     });
+//     cy.get('.btn.dark').click();
+    
+//     // Fill the form for goldenFarmers
+//     Object.keys(storedValue.goldenFarmers).forEach(key => {
+//       typeInInput(`#${key}`, storedValue.goldenFarmers[key]);
+//       cy.get(`#${key}`).should('have.value', storedValue.goldenFarmers[key]);
+//     });
+//     cy.get('.btn.dark').click();
+
+//     ['companyStrengths'].forEach(key => {
+//       typeInInput(`textarea[name=${key}]`, storedValue[key]);
+//       cy.get(`textarea[name=${key}]`).should('have.value', storedValue[key]);
+//     })
+//     cy.get('.btn.dark').click();
+
+//     // Fill the form for powerWords
+//     Object.keys(storedValue.powerWords).forEach(key => {
+//       typeInInput(`#${key}`, storedValue.powerWords[key]);
+//       cy.get(`#${key}`).should('have.value', storedValue.powerWords[key]);
+//     });
+//     cy.get('.btn.dark').click();
+
+//     // Fill the form for itemsProducts
+//     Object.keys(storedValue.itemsProducts).forEach(key => {
+//       typeInInput(`#${key}`, storedValue.itemsProducts[key]);
+//       cy.get(`#${key}`).should('have.value', storedValue.itemsProducts[key]);
+//     });
+//     cy.get('.btn.dark').click();
+//   });
+
+//   it('Verify that the Stepper component loads with all steps and titles rendered properly.', () => {
+//     titles.forEach((title, index) => {
+//       cy.contains(title).should('exist');
+//       if (index !== titles.length - 1) {
+//         cy.get('.btn.dark').click();
+//       }
+//     });
+//   });
+// });
+
+describe('Form Inputs:', () => {
   beforeEach(() => {
     cy.visit('/login');
     typeInInput('#email', email);
@@ -45,111 +175,40 @@ describe('Initial Load:', () => {
     cy.wait(10000).visit('business-sheet-creation');
   });
 
-  it('Verify that the BusinessSheetCreation page loads without any errors.', () => {
-    cy.url().should('include', 'business-sheet-creation');
-  });
+it('Verify that each step renders the appropriate input elements (TextArea, Input, UploadImage, Fonts, Themes).', () => {
+  // Check textareas for first several steps
+  for (let i = 0; i < 8; i++) {
+    clickAndCheckTextarea();
+  }
 
-  it('Verify that the form data is initialized correctly from local storage (or with default values if not present).', () => {
-    const formData = {
-      shortBiography,
-      businessDescription,
-      personalInformation,
-      goals,
-      accomplishments,
-      interests,
-      networks,
-      skills,
-      goldenEggs: {
-        goldenEgg0,
-        goldenEgg1,
-        goldenEgg2
-      },
-      goldenGooses: {
-        goldenGoose0,
-        goldenGoose1,
-        goldenGoose2
-      },
-      goldenFarmers: {
-        goldenFarmer0,
-        goldenFarmer1,
-        goldenFarmer2
-      },
-      companyStrengths,
-      powerWords: {
-        powerWords0,
-        powerWords1,
-        powerWords2,
-        powerWords3,
-        powerWords4,
-        powerWords5
-      },
-      itemsProducts: {
-        itemsProducts0,
-        itemsProducts1,
-        itemsProducts2
-      }
-    };
-    localStorage.setItem('businessFormData', JSON.stringify(formData));
-    cy.reload();
+  // Check goldenEgg inputs
+  checkInputs(['goldenEgg0', 'goldenEgg1', 'goldenEgg2']);
+  cy.get('.btn.dark').click();
 
-    const storedValue = JSON.parse(localStorage.getItem('businessFormData'));
-    cy.log(storedValue);
-    
-    // Fill the form for the main text fields
-    ['shortBiography', 'businessDescription', 'personalInformation', 'goals', 'accomplishments', 'interests', 'networks', 'skills'].forEach(key => {
-      typeInInput(`textarea[name=${key}]`, storedValue[key]);
-      cy.get(`textarea[name=${key}]`).should('have.value', storedValue[key]);
-      cy.get('.btn.dark').click();
-    });
-    
-    // Fill the form for goldenEggs
-    Object.keys(storedValue.goldenEggs).forEach(key => {
-      typeInInput(`#${key}`, storedValue.goldenEggs[key]);
-      cy.get(`#${key}`).should('have.value', storedValue.goldenEggs[key]);
-    });
-    cy.get('.btn.dark').click();
-    
-    // Fill the form for goldenGooses
-    Object.keys(storedValue.goldenGooses).forEach(key => {
-      typeInInput(`#${key}`, storedValue.goldenGooses[key]);
-      cy.get(`#${key}`).should('have.value', storedValue.goldenGooses[key]);
-    });
-    cy.get('.btn.dark').click();
-    
-    // Fill the form for goldenFarmers
-    Object.keys(storedValue.goldenFarmers).forEach(key => {
-      typeInInput(`#${key}`, storedValue.goldenFarmers[key]);
-      cy.get(`#${key}`).should('have.value', storedValue.goldenFarmers[key]);
-    });
-    cy.get('.btn.dark').click();
+  // Check goldenGoose inputs
+  checkInputs(['goldenGoose0', 'goldenGoose1', 'goldenGoose2']);
+  cy.get('.btn.dark').click();
 
-    ['companyStrengths'].forEach(key => {
-      typeInInput(`textarea[name=${key}]`, storedValue[key]);
-      cy.get(`textarea[name=${key}]`).should('have.value', storedValue[key]);
-    })
-    cy.get('.btn.dark').click();
+  // Check goldenFarmer inputs
+  checkInputs(['goldenFarmer0', 'goldenFarmer1', 'goldenFarmer2']);
+  cy.get('.btn.dark').click();
 
-    // Fill the form for powerWords
-    Object.keys(storedValue.powerWords).forEach(key => {
-      typeInInput(`#${key}`, storedValue.powerWords[key]);
-      cy.get(`#${key}`).should('have.value', storedValue.powerWords[key]);
-    });
-    cy.get('.btn.dark').click();
+  // Check textarea on the next step
+  clickAndCheckTextarea();
 
-    // Fill the form for itemsProducts
-    Object.keys(storedValue.itemsProducts).forEach(key => {
-      typeInInput(`#${key}`, storedValue.itemsProducts[key]);
-      cy.get(`#${key}`).should('have.value', storedValue.itemsProducts[key]);
-    });
-    cy.get('.btn.dark').click();
-  });
+  // Check powerWords inputs
+  checkInputs(['powerWords0', 'powerWords1', 'powerWords2', 'powerWords3', 'powerWords4', 'powerWords5']);
+  cy.get('.btn.dark').click();
 
-  it('Verify that the Stepper component loads with all steps and titles rendered properly.', () => {
-    titles.forEach((title, index) => {
-      cy.contains(title).should('exist');
-      if (index !== titles.length - 1) {
-        cy.get('.btn.dark').click();
-      }
-    });
-  });
+  // Check itemsProducts inputs
+  checkInputs(['itemsProducts0', 'itemsProducts1', 'itemsProducts2']);
+  cy.get('.btn.dark').click();
+
+  // Check for image upload and font/theme grids
+  cy.get('.headerBackgroundImage').should('exist');
+  cy.get('.profileImage').should('exist');
+  cy.get('.referralSheetBackgroundImage').should('exist');
+  cy.get('.fontsGrid .item').should('exist');
+  cy.get('.theme .item').should('exist');
 });
+})
