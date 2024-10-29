611c60c8986923418593af0700530b69c0ee70b6
Mohamad Ghaith Alzin <ghaith@Mohamads-MacBook-Pro.local>
2024-10-29 13:32:09 +0900

    add logout button

--- src/api/auth.js
+++ src/api/auth.js
@@ -21,6 +21,9 @@ export const registerUser = (userData) =>
 export const loginUser = (userData) =>
   handleApiResponse(() => secureApi.post(`${AUTH_URL}/login`, userData));
 
+export const logoutUser = (userData) => 
+  handleApiResponse(() => secureApi.post(`${AUTH_URL}/logout`, userData));
+
 // Should not be called with secureApi as users can't be authenticated when they forget their password.
 export const forgotPassword = (email) =>
   handleApiResponse(() =>
