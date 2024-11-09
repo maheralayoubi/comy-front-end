0110cf4c3e7b7682ad02af6a38252ba74eb27eec
Hakam-aldeen-Kh <hakamaldeenkh@outlook.com>
2024-11-09 19:00:54 +0300

    Test: Check that each member has: An image, name (e.g., “正岡夏門”), A category or job title (e.g., “UI/UX デザイン”).

--- cypress/e2e/Member-list page.cy.js
+++ cypress/e2e/Member-list page.cy.js
@@ -17,4 +17,12 @@ describe('Page Load and Basic Structure Verification:', () => {
   it('Confirm the page contains the heading “メンバー一覧”.', () => {
     cy.get('h2').should('be.visible').and('contain', 'メンバー一覧');
   });
+
+  it('Check that each member has: An image, name (e.g., “正岡夏門”), A category or job title (e.g., “UI/UX デザイン”).', () => {
+    cy.get('.member-list .user-card').each(($el, index) => {
+      cy.wrap($el).find('img').should('be.visible');
+      cy.wrap($el).find('.user-position').should('be.visible');
+      cy.wrap($el).find('.user-name').should('be.visible');
+    });
+  })
 })
