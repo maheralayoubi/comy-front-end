d1434a0842f1d0d2d9972bb098b0926d0bcf8b93
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2025-04-15 13:06:50 +0300

    adding TagManager

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "add-your-gtm-id-here-only-on-production",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
