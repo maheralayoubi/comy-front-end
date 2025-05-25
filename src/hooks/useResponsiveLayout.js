830c5886f11032e0c45e91bfc2564ad414cb086c
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-05-25 22:45:34 +0300

    fix back button in mobile view

--- src/hooks/useResponsiveLayout.js
+++ src/hooks/useResponsiveLayout.js
@@ -11,6 +11,7 @@ const safeSetStyle = (selector, property, value) => {
 };
 const useResponsiveLayout = (selectedChatId, setSelectedUserId) => {
   const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 991);
+
   const updateLayout = useCallback((isMobile, currentSelectedUserId) => {
     if (isMobile) {
       if (currentSelectedUserId) {
@@ -28,6 +29,7 @@ const useResponsiveLayout = (selectedChatId, setSelectedUserId) => {
       safeSetStyle(".profileDisplay", "display", "flex");
     }
   }, []);
+
   useEffect(() => {
     const handleResize = () => {
       const isMobile = window.innerWidth <= 991;
@@ -38,25 +40,31 @@ const useResponsiveLayout = (selectedChatId, setSelectedUserId) => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [selectedChatId, updateLayout]);
+
   useEffect(() => {
     if (isMobileView) {
       updateLayout(true, selectedChatId);
     }
   }, [selectedChatId, isMobileView, updateLayout]);
+
   const handleSelectUserMobile = useCallback(() => {
     if (isMobileView) {
       updateLayout(true, true);
     }
   }, [isMobileView, updateLayout]);
+
   const handleBackToListMobile = useCallback(() => {
     if (isMobileView) {
+      setSelectedUserId(null);
       updateLayout(true, null);
     }
-  }, [isMobileView,  updateLayout]);
+  }, [isMobileView, updateLayout, setSelectedUserId]);
+
   return {
     isMobileView,
     handleSelectUserMobile,
     handleBackToListMobile,
   };
 };
+
 export default useResponsiveLayout;
